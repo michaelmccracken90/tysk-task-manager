053252979d274adfc33af49b13e024b5b32839df
Thiaguz <thiagobruno.dev@gmail.com>
2020-07-10 12:18:36 -0300

    Add database_url in .env  Fix require ts-node/register only in development

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,15 +1,21 @@
 import { Config } from 'knex';
 
-require('ts-node/register');
+const {
+    NODE_ENV,
+    DATABASE_URL,
+    database_timezone,
+    database_client,
+    database_host,
+    database_name,
+    database_user,
+    database_password,
+} = process.env;
 
-module.exports = {
-    client: process.env.database_client || 'mysql2',
-    connection: {
-        host: process.env.database_host || '127.0.0.1',
-        database: process.env.database_name || 'tysk',
-        user: process.env.database_user || 'root',
-        password: process.env.database_password || '',
-    },
+if (NODE_ENV !== 'production') require('ts-node/register');
+
+const knexConfig = {
+    client: database_client || 'mysql2',
+
     pool: {
         min: 1,
         max: 1,
@@ -21,5 +27,16 @@ module.exports = {
     seeds: {
         directory: 'seeds',
     },
-    timezone: process.env.database_timezone || 'utc',
+    timezone: database_timezone || 'utc',
 } as Config;
+
+knexConfig.connection = DATABASE_URL
+    ? DATABASE_URL
+    : {
+          host: database_host || '127.0.0.1',
+          database: database_name || 'tysk',
+          user: database_user || 'root',
+          password: database_password || '',
+      };
+
+module.exports = knexConfig;

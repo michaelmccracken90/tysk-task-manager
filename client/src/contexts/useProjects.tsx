1b50a6697cc471e20655634a861a5c27527ed694
Thiaguz <thiagobruno.dev@gmail.com>
2020-07-18 19:29:15 -0300

    Add optimistic UI in useProjects and useTasks

--- client/src/contexts/useProjects.tsx
+++ client/src/contexts/useProjects.tsx
@@ -1,12 +1,18 @@
-import React, { createContext, useState, useContext, useEffect, useCallback } from 'react';
+import React, {
+    createContext,
+    useState,
+    useContext,
+    useEffect,
+    useCallback,
+} from 'react';
 import { ProjectsContextProps, Project } from '~/interfaces/api';
 import useAuth from './useAuth';
 import api from '~/services/axios';
 import { useAlert } from 'react-alert';
 
-const ProjectsContext = createContext<ProjectsContextProps>({} as ProjectsContextProps);
-
-// TODO: Should remove, add and update the projects from `projects` Array instead call indexProjects
+const ProjectsContext = createContext<ProjectsContextProps>(
+    {} as ProjectsContextProps
+);
 
 export const ProjectsProvider: React.FC = ({ children }) => {
     const [selectedProject, setSelectedProject] = useState({} as Project);
@@ -17,7 +23,8 @@ export const ProjectsProvider: React.FC = ({ children }) => {
 
     const indexProjects = useCallback(async () => {
         setLoading(true);
-        const results = (await api.get(`/users/${login?.user.id}/projects`)).data as Array<Project>;
+        const results = (await api.get(`/users/${login?.user.id}/projects`))
+            .data as Array<Project>;
 
         if (results.length >= 1) setSelectedProject(results[0]);
 
@@ -33,50 +40,70 @@ export const ProjectsProvider: React.FC = ({ children }) => {
 
     const createProjects = useCallback(
         async (title: string, description: string) => {
-            if (title && title.length > 25) return alert.error('Title too long.');
-            if (description && description.length > 126) return alert.error('Description too long.');
+            if (title && title.length > 25)
+                return alert.error('Title too long.');
+            if (description && description.length > 126)
+                return alert.error('Description too long.');
+
             setLoading(true);
+
             const results = (
                 await api.post('/users/projects', {
                     title,
                     description,
                 })
             ).data as Project;
-            setLoading(false);
-            await indexProjects();
-            setSelectedProject(results);
 
-            return results;
+            setProjects((p) => [...p, results]);
+
+            setLoading(false);
+
+            setSelectedProject(results);
         },
-        [indexProjects, alert],
+        [alert]
     );
 
     const updateProjects = async (project: Project) => {
-        if (project.title && project.title.length > 25) return alert.error('Title too long.');
-        if (project.description && project.description.length > 126) return alert.error('Description too long.');
-        setLoading(true);
-        const results = (await api.put(`/users/${login?.user.id}/projects/${selectedProject.id}`, project)).data as {
-            new: Project;
-            old: Project;
-        };
+        if (
+            project.title.replace(/\s/g, '').length < 3 ||
+            project.title.length > 25
+        )
+            return alert.error('Title too long or too short.');
+        if (project.description && project.description.length > 126)
+            return alert.error('Description too long.');
 
-        setLoading(false);
-        await indexProjects();
+        setProjects(
+            projects.map((indexedProject) => {
+                if (selectedProject.id === indexedProject.id) {
+                    const updatedProject = {
+                        ...indexedProject,
+                        title: project.title,
+                        description: project.description,
+                    };
 
-        setSelectedProject(results.new);
+                    setSelectedProject(updatedProject);
 
-        return results;
+                    return updatedProject;
+                }
+                return indexedProject;
+            })
+        );
+
+        api.put(
+            `/users/${login?.user.id}/projects/${selectedProject.id}`,
+            project
+        );
     };
 
     const deleteProjects = useCallback(async () => {
-        setLoading(true);
-        const results = (await api.delete(`/users/projects/${selectedProject.id}`)).data as Project;
-
-        setLoading(false);
-        await indexProjects();
-
-        return results;
-    }, [selectedProject, indexProjects]);
+        setProjects(
+            projects.filter(
+                (indexedProject) => selectedProject.id !== indexedProject.id
+            )
+        );
+        setSelectedProject(projects[0]);
+        api.delete(`/users/projects/${selectedProject.id}`);
+    }, [projects, selectedProject, setProjects, setSelectedProject]);
 
     return (
         <ProjectsContext.Provider
--- client/src/contexts/useTasks.tsx
+++ client/src/contexts/useTasks.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useState, useContext, useEffect, useCallback } from 'react';
+import React, {
+    createContext,
+    useState,
+    useContext,
+    useEffect,
+    useCallback,
+} from 'react';
 import { TasksContextProps, Task } from '~/interfaces/api';
 import useAuth from './useAuth';
 import api from '~/services/axios';
@@ -6,8 +12,6 @@ import useProjects from './useProjects';
 import { useAlert } from 'react-alert';
 
 const TasksContext = createContext<TasksContextProps>({} as TasksContextProps);
-
-// TODO: Should remove, add and update the projects from `projects` Array instead call indexTasks
 
 export const TasksProvider: React.FC = ({ children }) => {
     const { signed, login } = useAuth();
@@ -18,10 +22,14 @@ export const TasksProvider: React.FC = ({ children }) => {
 
     const indexTasks = useCallback(async () => {
         if (!selectedProject) return [];
+
         setLoading(true);
-        const results = (await api.get(`/users/${login?.user.id}/projects/${selectedProject.id}/tasks`)).data as Array<
-            Task
-        >;
+
+        const results = (
+            await api.get(
+                `/users/${login?.user.id}/projects/${selectedProject.id}/tasks`
+            )
+        ).data as Array<Task>;
 
         setTasks(results);
         setLoading(false);
@@ -35,50 +43,62 @@ export const TasksProvider: React.FC = ({ children }) => {
 
     const createTasks = useCallback(
         async (description: string) => {
-            if (description.length < 64) {
-                setLoading(true);
-                const results = (
-                    await api.post(`/users/${login?.user.id}/projects/${selectedProject.id}/tasks`, {
-                        description,
-                    })
-                ).data as Task;
-                setLoading(false);
-                await indexTasks();
+            if (description.length > 64) return alert.error('Task too long.');
+            setLoading(true);
 
-                return results;
-            } else {
-                alert.error('Task too long.');
-            }
+            const results = (
+                await api.post(
+                    `/users/${login?.user.id}/projects/${selectedProject.id}/tasks`,
+                    {
+                        description,
+                    }
+                )
+            ).data as Array<Task>;
+
+            setTasks((tasks) => [...tasks, results[0]]);
+
+            setLoading(false);
+
+            return results;
         },
-        [indexTasks, selectedProject, login, alert],
+        [selectedProject, login, alert]
     );
 
     const updateTasks = useCallback(
         async (task: Task) => {
-            if (task.description && task.description.length > 64) return alert.error('Task too long.');
-            setLoading(true);
-            const results = (
-                await api.put(`/users/${login?.user.id}/projects/${selectedProject.id}/tasks/${task.id}`, {
+            if (task.description && task.description.length > 64)
+                return alert.error('Task too long.');
+
+            setTasks((tasks) =>
+                tasks.map((indexedTask) => {
+                    if (indexedTask.id === task.id) {
+                        return {
+                            ...indexedTask,
+                            ...task,
+                        };
+                    }
+
+                    return indexedTask;
+                })
+            );
+
+            api.put(
+                `/users/${login?.user.id}/projects/${selectedProject.id}/tasks/${task.id}`,
+                {
                     description: task.description,
                     completed: task.completed || false,
-                })
-            ).data as { new: Task; old: Task };
-
-            setLoading(false);
-            await indexTasks();
-
-            return results;
+                }
+            );
         },
-        [login, selectedProject, indexTasks, alert],
+        [login, selectedProject, alert, setTasks]
     );
 
     const deleteTasks = async (taskId: number) => {
-        setLoading(true);
-        const results = (await api.delete(`/users/${login?.user.id}/projects/${selectedProject.id}/tasks/${taskId}`))
-            .data as Task;
-        setLoading(false);
-        await indexTasks();
-        return results;
+        setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+
+        api.delete(
+            `/users/${login?.user.id}/projects/${selectedProject.id}/tasks/${taskId}`
+        );
     };
 
     return (
